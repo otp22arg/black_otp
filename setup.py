697050d302b6dcb1e9a1c62f0a3627a50bbb6264
Sasha Hart <s@sashahart.net>
2015-01-04 19:16:13 -0600

    setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+
+from distutils.core import setup
+
+setup(
+    name="black_otp",
+    version="0.0.0",
+    author="Sasha Hart",
+    author_email="s@sashahart.net",
+    url="",
+    description="Reusable routines for decoding 'black OTP' messages from the OTP22 game",
+    license="MIT",
+    packages=["black_otp"],
+    scripts=[],
+    classifiers=[
+        "Remove this line in order to enable publication to PyPI"
+    ]
+)
